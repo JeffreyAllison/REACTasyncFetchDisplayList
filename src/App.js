9417161cd89a26ed2f1be34a227bdb0e2dafb9b3
JeffAllison <itsjallison@gmail.com>
2022-05-05 16:25:35 -0700

    imported SB data into app.js, created state and use effect function and state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { getAmps } from './services/fetch-utils';
+import { getWaterBodies } from './services/fetch-utils';
+import { getCandies } from './services/fetch-utils';
+import { getTeaKinds } from './services/fetch-utils';
 
 function App() {
+  const [amps, setAmps] = useState([]);
+  const [waterBodies, setWaterBodies] = useState([]);
+  const [candies, setCandies] = useState([]);
+  const [teaKinds, setTeaKinds] = useState([]);
+
+  useEffect(async () => {
+    const ampsResponse = await getAmps();
+    setAmps(ampsResponse);
+
+    const waterBodiesResponse = await getWaterBodies();
+    setWaterBodies(waterBodiesResponse);
+
+    const candiesResponse = await getCandies();
+    setCandies(candiesResponse);
+
+    const teaKindsResponse = await getTeaKinds();
+    setTeaKinds(teaKindsResponse);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
